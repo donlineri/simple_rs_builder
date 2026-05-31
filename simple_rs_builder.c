b9dc43136a37fcbdbbfaa826cff57546c0781371
Danil Ivantsov <berserk357@gmail.com>
2026-05-31 07:47:50 +0400

    added input of some function for maximization

--- simple_rs_builder.c
+++ simple_rs_builder.c
@@ -19,10 +19,10 @@ enum {
 	max_length_line = 255,
 };
 
-typedef struct sup_f_s {
+typedef struct func2_s {
 	te_expr *expr;
-	double phi1, phi2;
-} sup_f;
+	double x, y;
+} func2;
 
 typedef struct func1_s {
 	te_expr *expr;
@@ -33,7 +33,7 @@ typedef struct problem_s {
 	func1 **a;
 	double *x0, t, **phi;
 	int n;
-	sup_f c_u;
+	func2 c_u, f;
 } problem;
 
 void draw_cp(unsigned int VAO, int count_vertices, unsigned int shader_id)
@@ -217,43 +217,49 @@ char *get_stroka(char *buf)
 	return s;
 }
 
-te_expr *get_func(char *s, te_variable *vars, int te_vars_count)
+te_expr *get_func(char *buf, te_variable *vars, int te_vars_count)
 {
 	int err;
-	char input[max_length_line];
-	te_expr *expr;
-	while(isspace(*s))
-		s++;
-	expr = te_compile(s, vars, te_vars_count, &err);
+	char *s;
+	te_expr *expr = NULL;
 	while(!expr) {
-		if(err != 1)
-			fprintf(stderr, "Parse error at %d\n", err);
-		s = get_stroka(input);
+		s = get_stroka(buf);
+		while(isspace(*s))
+			s++;
+		if(strlen(s) == 0)
+			continue;
+		if(*s == '#')
+			continue;
 		expr = te_compile(s, vars, te_vars_count, &err);
+		if(!expr) {
+			fprintf(stderr, "Parse error at %d\n", err);
+		}
 	}
 	return expr;
 }
 
-void parse_input(func1 ***a, double **x0, double *t, int *n, sup_f *c_u)
+void parse_input(func1 ***a, double **x0, double *t, int *n, func2 *c_u,
+		func2 *f)
 {
 	const int dim = 2;
 	const int num_count = 4;
-	int i, j, pos, te_vars_count_c_u, te_vars_count_aij;
+	int i, j, pos, te_vars_count_c_u, te_vars_count_aij, te_vars_count_f;
 	char input[max_length_line], *s, *endptr;
 	double data[num_count];
 	te_variable vars_c_u[] = {{"max", (void*) max,
-		TE_FUNCTION2}, {"phi1", &c_u->phi1}, {"phi2", &c_u->phi2}};
+		TE_FUNCTION2}, {"phi1", &c_u->x}, {"phi2", &c_u->y}};
 	(*a) = malloc(sizeof(func1 *) * dim);
 	(*a)[0] = malloc(sizeof(func1) * dim);
 	(*a)[1] = malloc(sizeof(func1) * dim);
 	te_variable vars_aij[2][2][2] = {{{{"t", &(*a)[0][0].t}},
 		{{"t", &(*a)[1][0].t}}}, {{{"t", &(*a)[0][1].t}}, {{"t", &(*a)[1][1].t}}}};
+	te_variable vars_f[] = {{"x", &f->x}, {"y", &f->y}};
 	te_vars_count_c_u = 3;
 	te_vars_count_aij = 1;
+	te_vars_count_f = 2;
 	for(i = 0; i < dim; i++)
 		for(j = 0; j < dim; j++) {
-			s = get_stroka(input);
-			(*a)[j][i].expr = get_func(s, vars_aij[i][j], te_vars_count_aij);
+			(*a)[j][i].expr = get_func(input, vars_aij[i][j], te_vars_count_aij);
 		}
 	*x0 = malloc(sizeof(double)*dim);
 	pos = 0;
@@ -271,7 +277,8 @@ void parse_input(func1 ***a, double **x0, double *t, int *n, sup_f *c_u)
 		}
 	}
 
-	c_u->expr = get_func(s, vars_c_u, te_vars_count_c_u);
+	c_u->expr = get_func(input, vars_c_u, te_vars_count_c_u);
+	f->expr = get_func(input, vars_f, te_vars_count_f);
 	
 	/*
 	for(i = 0; i < dim; i++)
@@ -292,7 +299,8 @@ void freemat(double **m, int size)
   free(m);
 }
 
-void print_input(func1 ***a, double **x0, double *t, int *n, sup_f *c_u)
+void print_input(func1 ***a, double **x0, double *t, int *n, func2 *c_u,
+		func2 *f)
 {
 	int i, j;
 	printf("MATRIX A\n");
@@ -301,12 +309,15 @@ void print_input(func1 ***a, double **x0, double *t, int *n, sup_f *c_u)
 			(*a)[i][j].t = 1.0;
 			printf("a[%d][%d](%lf) = %lf\n", i, j, (*a)[i][j].t, te_eval((*a)[i][j].expr));
 		}
-	(*c_u).phi1 = 1;
-	(*c_u).phi2 = 1;
-	printf("f(%lf,%lf) = %lf\n", (*c_u).phi1, (*c_u).phi2, te_eval((*c_u).expr));
-	(*c_u).phi1 = 3;
-	(*c_u).phi2 = -2;
-	printf("f(%lf,%lf) = %lf\n", (*c_u).phi1, (*c_u).phi2, te_eval((*c_u).expr));
+	(*c_u).x = 1;
+	(*c_u).y = 1;
+	printf("c_u(%lf,%lf) = %lf\n", (*c_u).x, (*c_u).y, te_eval((*c_u).expr));
+	(*c_u).x = 3;
+	(*c_u).y = -2;
+	printf("c_u(%lf,%lf) = %lf\n", (*c_u).x, (*c_u).y, te_eval((*c_u).expr));
+	(*f).x = 1;
+	(*f).y = 1;
+	printf("f(%lf,%lf) = %lf\n", (*f).x, (*f).y, te_eval((*f).expr));
 	printf("VECTOR x0\n");
 	printvec(*x0, 2);
 	printf("T = %lf\n", *t);
@@ -357,9 +368,9 @@ void runge_kutta(problem *p, int j, double *x, int xsize, double *y1,
 void integration_by_rectangle_h(problem *p, int j, double h,
 		double *integral, double *psi_t)
 {
-  int node_count, i, psi_node_count, a = 0, b = p->t;
-  double sum = 0.0, *t, *psi1, *psi2, psi_h;
-	sup_f *c_u = &p->c_u;
+  int node_count, i, psi_node_count;
+  double sum = 0.0, *t, *psi1, *psi2, psi_h, a = 0, b = p->t;
+	func2 *c_u = &p->c_u;
 	node_count = (int)trunc((b-a)/h)+1;
 	h = (b-a)/(double)node_count;
 	node_count++;
@@ -372,8 +383,8 @@ void integration_by_rectangle_h(problem *p, int j, double h,
 		t[i] = a + i*psi_h;
 	runge_kutta(p, j, t, psi_node_count, psi1, psi2);
 	for(i = 1; i < node_count; i++) {
-		c_u->phi1 = psi1[2*i-1];
-		c_u->phi2 = psi2[2*i-1];
+		c_u->x = psi1[2*i-1];
+		c_u->y = psi2[2*i-1];
 		sum += te_eval(c_u->expr);
 	}
 	psi_t[0] = psi1[psi_node_count-1];
@@ -396,6 +407,10 @@ double calc_c_j(problem *p, int j, double eps)
   }
   //printf("delta = %.128lf\n", delta);
   //printf("h = %lf\n", h);
+	//printf("1: %lf\n", dot_product(psi_t, p->x0, 2));
+	//printf("2: %lf\n", i_half_h);
+	//printf("3: %lf\n", delta);
+	//exit(1);
   return dot_product(psi_t, p->x0, 2)+i_half_h+delta;
 }
 
@@ -404,7 +419,6 @@ void get_omega(problem *p, double ***omega, int *omega_size)
 	int i, j, n;
 	double *c;
 	n = p->n;
-	//print_input(&a, &x0, &t, &n, &c_u);
 	c = (double *) malloc(sizeof(double) * n);
 	for(j = 0; j < n; j++)
 		c[j] = calc_c_j(p, j, 0.001);
@@ -423,33 +437,36 @@ void get_omega(problem *p, double ***omega, int *omega_size)
 	free(c);
 }
 
-void get_dots_pos(double *cp_v, int cp_v_count,
+void get_dots_pos(double *cp_v, int cp_v_count, func2 *f,
 		vec3 **dots_p, int *dots_p_count)
 {
-	int i, j, *cp_num_v, max_count, is_checked;
+	int i, j, *cp_num_v, max_count;
 	double *cp_dist_v;
 	cp_dist_v = malloc(sizeof(double) * cp_v_count);
 	cp_num_v = malloc(sizeof(int) * cp_v_count);
 	for(i = 0; i < cp_v_count; i++) {
-		cp_dist_v[i] = cp_v[i*6]*cp_v[i*6] + cp_v[i*6+1]*cp_v[i*6+1];
+		f->x = cp_v[i*6];
+		f->y = cp_v[i*6+1];
+		cp_dist_v[i] = te_eval(f->expr);
 		cp_num_v[i] = i;
 	}
 	sort(cp_num_v, cp_dist_v, cp_v_count);
-	max_count = 0;
-	is_checked = 0;
+	max_count = 1;
 	for(i = cp_v_count - 1; i > 0; i--)
 		if(fabs(cp_dist_v[i] - cp_dist_v[i-1]) < ALMOSTZERO)
 			max_count++;
-		else {
+		else
 			break;
-			is_checked = 1;
-		}
-	if(!is_checked && (fabs(cp_dist_v[1] - cp_dist_v[0]) < ALMOSTZERO))
-		max_count++;
+
+	/*
 	for(j = 0; j < max_count; j++) {
 		i = cp_num_v[cp_v_count - 1 - j];
 		printf("extreme dot (x,y) = (%lf,%lf)\n", cp_v[i*6], cp_v[i*6+1]);
+		f->x = cp_v[i*6];
+		f->y = cp_v[i*6+1];
+		printf("f(%lf,%lf) = %lf\n", (*f).x, (*f).y, te_eval((*f).expr));
 	}
+	*/
 
 	*dots_p = malloc(sizeof(vec3) * max_count);
 	*dots_p_count = max_count;
@@ -487,6 +504,7 @@ void deinit_problem(problem *p)
 	free(p->a);
 	free(p->x0);
 	te_free(p->c_u.expr);
+	te_free(p->f.expr);
 }
 
 void process_input(GLFWwindow *window, int *is_animation, int
@@ -498,13 +516,73 @@ void process_input(GLFWwindow *window, int *is_animation, int
 		*is_extreme_problem = 1;
 }
 
-int main()
+void get_rs(problem *p, unsigned int *VBO, unsigned int *VAO,
+		int *cp_count_vertices, vec3 **dots_pos, int *dots_pos_count)
+{
+	int omega_size, i;
+	double **omega, *cp_data;
+	double prev_t, cur_t;
+
+	prev_t = glfwGetTime();
+	get_omega(p, &omega, &omega_size);
+	cur_t = glfwGetTime();
+	printf("DEBUG: get omega in H-format time: %lf\n", cur_t-prev_t);
+
+	prev_t = glfwGetTime();
+	get_cp_vertices(omega, omega_size, &cp_data, cp_count_vertices);
+	get_dots_pos(cp_data, *cp_count_vertices, &p->f, dots_pos, dots_pos_count);
+	prepare_object(VBO, VAO, *cp_count_vertices, cp_data);
+	cur_t = glfwGetTime();
+	printf("DEBUG: sort cp_data, solve extreme problem & create cp buffers: "
+			"%lf\n", cur_t-prev_t);
+
+	freemat(omega, omega_size);
+	free(cp_data);
+	printf("cp_count_vertices: %d\n", *cp_count_vertices);
+	printf("dots_pos_count: %d\n", *dots_pos_count);
+	printf("maxima\n");
+	if(*dots_pos_count < 5) {
+		for(i = 0; i < *dots_pos_count; i++)
+			printf("(%lf,%lf)\n", (*dots_pos)[i][0], (*dots_pos)[i][1]);
+	}
+	else {
+		for(i = 0; i < 5; i++)
+			printf("(%lf,%lf)\n", (*dots_pos)[i][0], (*dots_pos)[i][1]);
+		printf("and so on... (%d) \n", *dots_pos_count - 5);
+	}
+}
+
+void parse_cmdline(int argc, char **argv, coordplane *plane)
+{
+	int i;
+	char *s, *endptr;
+	if(argc > 1)
+		for(i = 1; i < argc; i++) {
+			s = argv[i];
+			while(isspace(*s))
+				s++;
+			if(strlen(s) == 0)
+				continue;
+			if(strcmp("-clip", s) == 0) {
+				if(i == argc-1) {
+					fprintf(stderr, "Error: clip requires an argument\n");
+					exit(1);
+				}
+				plane->clip = strtof(argv[i+1], &endptr);
+				if(endptr == argv[i+1]) {
+					fprintf(stderr, "Error: you must specify a float\n");
+					exit(1);
+				}
+			}
+		}
+}
+
+int main(int argc, char **argv)
 {
 	coordplane *plane;
 	unsigned int cp_VBO, cp_VAO, dot_VBO, dot_VAO;
-	int omega_size, cp_count_vertices, dots_pos_count, is_animation = 0,
+	int cp_count_vertices, dots_pos_count, is_animation = 0,
 			is_extreme_problem = 0;
-	double *cp_data, **omega, prev_t, cur_t;
 	vec3 *dots_pos;
 	const double dot_data[24] = {
 		1.0, -1.0, 0, .322, .576, .839,
@@ -514,58 +592,28 @@ int main()
 	};
 	problem p;
 
-	parse_input(&p.a, &p.x0, &p.t, &p.n, &p.c_u);
-	//print_input(&p.a, &p.x0, &p.t, &p.n, &p.c_u);
+	parse_input(&p.a, &p.x0, &p.t, &p.n, &p.c_u, &p.f);
+	//print_input(&p.a, &p.x0, &p.t, &p.n, &p.c_u, &p.f);
 	coordplane_create(&plane);
 	get_direction(&p.phi, p.n);
   dd_set_global_constants(); /* First, this must be called once to use cddlib. */
-
-	get_omega(&p, &omega, &omega_size);
-	get_cp_vertices(omega, omega_size, &cp_data, &cp_count_vertices);
-	freemat(omega, omega_size);
-	get_dots_pos(cp_data, cp_count_vertices, &dots_pos, &dots_pos_count);
-	prepare_object(&cp_VBO, &cp_VAO, cp_count_vertices, cp_data);
-	free(cp_data);
 	prepare_object(&dot_VBO, &dot_VAO, 4, dot_data);
+	parse_cmdline(argc, argv, plane);
+
+	get_rs(&p, &cp_VBO, &cp_VAO, &cp_count_vertices, &dots_pos,
+			&dots_pos_count);
 
 	while(!glfwWindowShouldClose(plane->window))
 	{
 		if(is_animation) {
 			p.t += 0.1f;
 			printf("t: %f\n", p.t);
-			printf("cp_count_vertices: %d\n", cp_count_vertices);
 
-			prev_t = glfwGetTime();
 			delete_object(cp_VBO, cp_VAO);
 			free(dots_pos);
-			cur_t = glfwGetTime();
-			printf("DEBUG: delete_object time: %lf\n", cur_t-prev_t);
 
-			prev_t = glfwGetTime();
-			get_omega(&p, &omega, &omega_size);
-			cur_t = glfwGetTime();
-			printf("DEBUG: get_omega time: %lf\n", cur_t-prev_t);
-
-			prev_t = glfwGetTime();
-			get_cp_vertices(omega, omega_size, &cp_data, &cp_count_vertices);
-			cur_t = glfwGetTime();
-			printf("DEBUG: get_cp_vertices time: %lf\n", cur_t-prev_t);
-
-			prev_t = glfwGetTime();
-			freemat(omega, omega_size);
-			cur_t = glfwGetTime();
-			printf("DEBUG: freemat time: %lf\n", cur_t-prev_t);
-
-			prev_t = glfwGetTime();
-			get_dots_pos(cp_data, cp_count_vertices, &dots_pos, &dots_pos_count);
-			cur_t = glfwGetTime();
-			printf("DEBUG: get_dots_pos time: %lf\n", cur_t-prev_t);
-
-			prev_t = glfwGetTime();
-			prepare_object(&cp_VBO, &cp_VAO, cp_count_vertices, cp_data);
-			free(cp_data);
-			cur_t = glfwGetTime();
-			printf("DEBUG: prepare_object time: %lf\n", cur_t-prev_t);
+			get_rs(&p, &cp_VBO, &cp_VAO, &cp_count_vertices, &dots_pos,
+					&dots_pos_count);
 
 			is_animation = 0;
 		}
@@ -584,6 +632,7 @@ int main()
 	}
 	delete_object(cp_VBO, cp_VAO);
 	free(dots_pos);
+
 	delete_object(dot_VBO, dot_VAO);
 	coordplane_delete(plane);
 	deinit_problem(&p);
